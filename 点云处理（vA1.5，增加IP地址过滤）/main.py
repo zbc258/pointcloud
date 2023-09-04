6f40b44d8e3c0396b8edaa3cde6756eb9bb2d7a6
zbc258 <91818523+zbc258@users.noreply.github.com>
2023-09-04 22:42:24 +0800

    Delete 点云处理（vA1.5，增加IP地址过滤） directory

--- 点云处理（vA1.5，增加IP地址过滤）/main.py
+++
@@ -1,9 +0,0 @@
-﻿
-
-import combine_process
-
-if __name__ == '__main__':
-    r_path = "C:/Users/22521/Desktop/现场数据/14.pcapng"
-    new_path="C:/Users/22521/Desktop/test.txt"
-    combine_process.pack_combine(r_path,new_path)
-    #read_show(new_path)
